3572ca18f5ab63817415c05e47f7bf001c15363f
yooda123 <y0o0y321@gmail.com>
2022-01-19 01:09:54 +0900

    hello, assignment, built in function

---
+++ builtin_func.py
@@ -0,0 +1,18 @@
+# built in function
+
+# type()
+hello_type = type("Hello")
+print(hello_type)
+
+print(type(1))
+
+
+# id()
+hello_id = id("hello")
+print(hello_id)
+
+hello = "hello"
+hello2 = "hello"
+
+print(id(hello))
+print(id(hello2))
